5dcd701a46a5a92aeed2d58a8cf53579e9c0a2d0
Konstantin Agapov <agapovk@MacBook-Air-Konstantin.local>
2023-01-09 23:00:26 +0500

    fix login validation

--- frontend/src/schemas/login.js
+++ frontend/src/schemas/login.js
@@ -4,7 +4,7 @@ import * as yup from 'yup';
 
 const loginSchema = yup.object().shape({
 	name: yup.string().min(3, 'От 3 до 20 символов').required('Обязательное поле'),
-	password: yup.string().min(5, 'Минимум 5 символов').required('Обязательное поле')
+	password: yup.string().required('Обязательное поле')
 });
 
 export default loginSchema;
